09743f832cb3d5a38a32f07b2d357b620f68a7b5
Daniel Echeverry Giraldo <dfecheverryg@unal.edu.co>
2018-01-21 21:48:25 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,6 +1,6 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
 
-def display_board 
+def display_board
   fila = "   |   |   "
   espacio = "-----------"
   puts fila
@@ -9,3 +9,5 @@ def display_board
   puts espacio
   puts fila
 end
+
+display_board
